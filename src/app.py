2c8d91cfa61a32a8ddfd395c5adfea21e38bac9f
omararcx <100163833+omararcx@users.noreply.github.com>
2023-11-03 21:14:19 +0000

    created models (again) and first endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,40 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route ('/people', methods=['GET'])
+def get_people():
+    people = People()
+    people = people.query.all()
+    return jsonify(list(map(lambda item : item.serialize(), people))), 200
+
+@app.route ('/people/<int:theid>', methods=['GET'])
+def get_character(theid=None):
+    if theid is None:
+        return ({"message": "the id is necessary"}), 404
+    character = People()
+    character = character.query.get(theid)
+    if character is None:
+        return ({"message": "character doesn't exist"})
+    return jsonify((character.serialize())), 200
+
+@app.route ('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets()
+    planets = planets.query.all()
+    return jsonify(list(map(lambda item : item.serialize(), planets))), 200
+
+@app.route ('/planets/<int:theid>', methods=['GET'])
+def get_planet(theid=None):
+    if theid is None:
+        return ({"message": "the id is necessary"}), 404
+    planet = Planets()
+    planet = planet.query.get(theid)
+    if planet is None:
+        return ({"message": "planet doesn't exist"})
+    return jsonify((planet.serialize())), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,80 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class User(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    username=db.Column(db.String(30), nullable=False, unique=True)
+    name =db.Column(db.String(80), nullable=False)
+    lastname=db.Column(db.String(80), nullable=False)
+    age=db.Column(db.Integer(), nullable=True)
+    email=db.Column(db.String(80), nullable=True, unique=True)
+    country=db.Column(db.String(60), nullable=True)
+    favorites = db.relationship("Favorite", uselist=True, backref='user')
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age,
             "email": self.email,
+            "country": self.country,
+            "favorites": list(map(lambda item: item.serialize(), self.favorite))
+        }
+      
+
+
+class People(db.Model):
+    id= db.Column(db.Integer(), primary_key=True)
+    name= db.Column(db.String(80), nullable=False)
+    side= db.Column(db.String(30), nullable=True)
+    status= db.Column(db.String(30), nullable=True)
+    origin= db.Column(db.String(30), nullable=True)
+    favorites= db.relationship("Favorite", backref='people')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "side": self.side,
+            "status": self.status, 
+            "origin": self.origin,
+        }
+
+class Planets(db.Model):
+    id=db.Column(db.Integer(), primary_key=True, nullable=False)
+    name=db.Column(db.String(150), nullable=False)
+    population=db.Column(db.Integer(), nullable=True)
+    controled=db.Column(db.String(150), nullable=True)
+    terrain = db.Column(db.String(120), nullable=False)
+    favorites=db.relationship("Favorite", backref="planets")
+
+    def serialize(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+                "population": self.population, 
+                "controles": self.controled,
+                "terrain" : self.terrain
+            }
+        
+
+class Favorite(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    user_id=db.Column(db.Integer(),db.ForeignKey("user.id"))
+    planet_id=db.Column(db.Integer(),db.ForeignKey("planets.id"))
+    people_id=db.Column(db.Integer(),db.ForeignKey("people.id"))
+
+    def serialize(self):
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "people_id": self.people_id,
+                "planet_id": self.planet_id,
             # do not serialize the password, its a security breach
+
         }
